c695b4278466d7840ce62185da0c91b9e4e15808
Brie Elmassian <brieelmassian@Bries-MacBook.local>
2017-06-24 11:15:52 -0400

    added firebase

---
+++ groupproject.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAmzWqyKDVOt80b43DplFYGdD1ChLga6xk",
+  authDomain: "groupproject1-86e02.firebaseapp.com",
+  databaseURL: "https://groupproject1-86e02.firebaseio.com",
+  projectId: "groupproject1-86e02",
+  storageBucket: "",
+  messagingSenderId: "555005447916"
+  };
+firebase.initializeApp(config);
